17d1dd52407ef128b21171bd11f9e3ac0f89a305
Spartiegirl <42212891+Spartiegirl@users.noreply.github.com>
2025-04-09 11:37:45 -0400

    created js, updated html

---
+++ src/script.js
@@ -0,0 +1,22 @@
+
+function updateTime() {
+
+let philadelphiaElement = document.querySelector("#philadelphia");
+let philadelphiaDateElement = philadelphiaElement.querySelector(".date");
+let philadelphiaTimeElement = philadelphiaElement.querySelector(".time");
+let philadelphiaTime = moment().tz("America/Philadelphia");
+philadelphiaDateElement.innerHTML = philadelphiaTime.format("MMMM Do, YYYY");
+philadelphiaTimeElement.innerHTML = philadelphiaTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+let sydneyElement = document.querySelector("#sydney");
+let sydneyDateElement = sydneyElement.querySelector(".date");
+let sydneyTimeElement =sydneyElement.querySelector(".time");
+let sydneyTime = moment().tz("Australia/Sydney");
+sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
+sydneyTimeElement.innerHTML = sydneyTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+}
+updateTime();
+setInterval(updateTime, 1000);
